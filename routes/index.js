06de63baa2398af17ecc7aab23679288d5d2dbc0
JialiuWuxi <37652140+JialiuWuxi@users.noreply.github.com>
2018-08-14 11:54:51 +0800

    INIT

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var authHelper = require('../helpers/auth');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let parms = { title: 'Home', active: { home: true } };
+
+  const accessToken = req.cookies.graph_access_token;
+  const userName = req.cookies.graph_user_name;
+
+  if (accessToken && userName) {
+    parms.user = userName;
+    parms.debug = `User: ${userName}\nAccess Token: ${accessToken}`;
+  } else {
+    parms.signInUrl = authHelper.getAuthUrl();
+    parms.debug = parms.signInUrl;
+  }
+
+  res.render('index', parms);
 });
 
 module.exports = router;
+
+
